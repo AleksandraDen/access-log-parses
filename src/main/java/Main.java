b14a14fa420605993b45536c84465ff5ea85cd6e
Alexandra Denisova <ADenisova@inno.tech>
2025-11-09 12:32:22 +0300

    Добавлены комментарии в выводимые данные

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число");
+        int secondNumber = new Scanner(System.in).nextInt();
+        double quotient = (double) firstNumber/secondNumber;
+        System.out.println("Результат деления введенных вами чисел: " + quotient);
+        int sum = firstNumber + secondNumber;
+        System.out.println("Результат сложения введенных вами чисел: " + sum);
+        int subtraction = secondNumber - firstNumber;
+        System.out.println("Результат вычитания введенных вами чисел: " + subtraction);
+        int multiply = firstNumber * secondNumber;
+        System.out.println("Результат умножения введенных вами чисел: " + multiply);
+
     }
 }
